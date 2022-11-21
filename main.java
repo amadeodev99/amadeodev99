91c41fcca7d375c38f3b199df1488830ac924899
amadeodev99 <117880512+amadeodev99@users.noreply.github.com>
2022-11-21 20:58:02 +0000

    Pending changes exported from your codespace

---
+++ main.java
@@ -0,0 +1,28 @@
+// JDK INSTALLATION
+// ctrl + shift + p --> >dev container
+//https://docs.github.com/en/codespaces/setting-up-your-project-for-codespaces/setting-up-your-project-for-codespaces?langId=java
+
+public class main {
+    public static void main(String[] args) {
+
+        //print
+        System.out.println("Print Line"); //print
+
+        //text variable
+        String text = "Hello"; //text variable
+        System.out.println(text); //print text variable
+
+        //number variable
+        int number = 7; //number variable
+        System.out.println(number); //print number variable
+
+        float decimal = 3.8f; //decimal variable
+        double longNumber = 3; //decimal variable [second version]
+
+        System.out.println(decimal + longNumber); //print decimal variable
+
+
+
+
+      }
+}
